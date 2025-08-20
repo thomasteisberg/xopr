f440cb4398f272ddf4ae995dfc90670fe412ae19
Thomas Teisberg <tteisberg@gmail.com>
2025-08-19 18:01:13 -0700

    Basic unit tests for data loading

--- src/xopr/matlab_attribute_utils.py
+++ src/xopr/matlab_attribute_utils.py
@@ -7,7 +7,7 @@ import numpy as np
 # HDF5-format MATLAB files
 #
 
-def dereference_h5value(value, h5file):
+def dereference_h5value(value, h5file, make_array=True):
     if isinstance(value, h5py.Reference):
         return dereference_h5value(h5file[value], h5file=h5file)
     elif isinstance(value, h5py.Group):
@@ -15,9 +15,12 @@ def dereference_h5value(value, h5file):
         return decode_hdf5_matlab_variable(value, h5file=h5file)
     elif isinstance(value, Iterable):
         v = [dereference_h5value(v, h5file=h5file) for v in value]
-        try:
-            return np.squeeze(np.array(v))
-        except:
+        if make_array:
+            try:
+                return np.squeeze(np.array(v))
+            except:
+                return v
+        else:
             return v
     elif isinstance(value, np.number):
         return value.item()
@@ -34,7 +37,7 @@ def decode_hdf5_matlab_variable(h5var, skip_variables=False, debug_path="", skip
     matlab_class = h5var.attrs.get('MATLAB_class', None)
     
     if matlab_class and matlab_class == b'cell':
-        return dereference_h5value(h5var[:], h5file=h5file)
+        return dereference_h5value(h5var[:], h5file=h5file, make_array=False)
     elif matlab_class and matlab_class == b'char':
         return h5var[:].astype(dtype=np.uint8).tobytes().decode('utf-8')
     elif isinstance(h5var, (h5py.Group, h5py.File)):
@@ -56,15 +59,20 @@ def decode_hdf5_matlab_variable(h5var, skip_variables=False, debug_path="", skip
             else:
                 attrs[k] = decode_hdf5_matlab_variable(h5var[k], debug_path=debug_path + "/" + k, skip_errors=skip_errors, h5file=h5file)
         return attrs
+    elif isinstance(h5var, h5py.Dataset):
+        if h5var.dtype == 'O':
+            return dereference_h5value(h5var[:], h5file=h5file)
+        else:
+            return np.squeeze(h5var[:])
     else:
-        return np.squeeze(h5var[:])
+        return h5var[:]
 
 #
 # Legacy MATLAB files (non-HDF5)
 #
 
 def extract_legacy_mat_attributes(file, skip_keys=[], skip_errors=True):
-    m = scipy.io.loadmat(file, mat_dtype=True, simplify_cells=True, squeeze_me=True)
+    m = scipy.io.loadmat(file, mat_dtype=False, simplify_cells=True, squeeze_me=True)
 
     attrs = {}
     for key, value in m.items():
@@ -73,7 +81,9 @@ def extract_legacy_mat_attributes(file, skip_keys=[], skip_errors=True):
         else:
             attrs[key] = value
 
-    return strip_api_key(attrs)
+    attrs = strip_api_key(attrs)
+    attrs = convert_object_ndarrays_to_lists(attrs)
+    return attrs
 
 def strip_api_key(attrs):
     attrs_clean = {}
@@ -85,3 +95,16 @@ def strip_api_key(attrs):
         else:
             attrs_clean[key] = value
     return attrs_clean
+
+def convert_object_ndarrays_to_lists(attrs):
+    """
+    Convert any object ndarray attributes to lists.
+    """
+    for key, value in attrs.items():
+        if isinstance(value, np.ndarray) and value.dtype == 'object':
+            attrs[key] = value.tolist()
+        elif isinstance(value, dict):
+            convert_object_ndarrays_to_lists(value)
+        else:
+            attrs[key] = value
+    return attrs
